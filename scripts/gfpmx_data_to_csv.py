555ecc510a2b5a9ac06d8ba0ec61a32c00e7411b
Paul Rougieux <paul.rougieux@gmail.com>
2021-08-17 18:22:19 +0200

    Write excel to csv

---
+++ scripts/gfpmx_data_to_csv.py
@@ -0,0 +1,42 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A script to save all sheets of the GFPMX Excel implementation to csv files
+
+Author Paul Rougieux
+
+See also the notebook that explores this data.
+
+"""
+
+# Built-in modules #
+from pathlib import Path
+
+# Third party modules #
+import pandas
+from tqdm import tqdm
+
+# First party modules #
+
+# Internal modules #
+from gftmx.data_dir import gftmx_data_dir
+
+# Input file
+excel_file = "~/large_models/GFPMX-8-6-2021.xlsx"
+# Output folder
+gfpmx_data_dir = gftmx_data_dir + "gfpmx"
+if not Path(gfpmx_data_dir).exists():
+    Path(gfpmx_data_dir).mkdir(parents=True)
+
+# Load the Excel file in a dictionary of data frames
+gfpmx_data = pandas.read_excel(excel_file, sheet_name = None)
+print(gfpmx_data.keys())
+
+# Industrial roundwood production
+gfpmx_data['IndroundProd'].to_csv(data_dir + "/" + "IndroundProd.csv")
+
+# Write all sheets to csv files
+for key in tqdm(gfpmx_data.keys()):
+    gfpmx_data[key].to_csv(gfpmx_data_dir + "/" + key + ".csv", index=False)
