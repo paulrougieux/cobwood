66876627a69877d015f63fefe2780b301ee9b541
Paul Rougieux <paul.rougieux@gmail.com>
2021-08-19 16:06:37 +0200

    Remove empty columns before writing the sheet to a csv file

--- scripts/gfpmx_data_to_csv.py
+++ scripts/gfpmx_data_to_csv.py
@@ -7,12 +7,13 @@ A script to save all sheets of the GFPMX Excel implementation to csv files
 
 Author Paul Rougieux
 
-See also the notebook that explores this data.
+See also the notebook that reproduces GFPM simulation.
 
 """
 
 # Built-in modules #
 from pathlib import Path
+import os
 
 # Third party modules #
 import pandas
@@ -26,17 +27,21 @@ from gftmx.data_dir import gftmx_data_dir
 # Input file
 excel_file = "~/large_models/GFPMX-8-6-2021.xlsx"
 # Output folder
-gfpmx_data_dir = gftmx_data_dir + "gfpmx"
+gfpmx_data_dir = os.path.join(gftmx_data_dir, "gfpmx")
 if not Path(gfpmx_data_dir).exists():
     Path(gfpmx_data_dir).mkdir(parents=True)
 
 # Load the Excel file in a dictionary of data frames
-gfpmx_data = pandas.read_excel(excel_file, sheet_name = None)
+gfpmx_data = pandas.read_excel(excel_file, sheet_name=None)
 print(gfpmx_data.keys())
 
 # Industrial roundwood production
-gfpmx_data['IndroundProd'].to_csv(data_dir + "/" + "IndroundProd.csv")
+gfpmx_data['IndroundProd'].to_csv(gfpmx_data_dir + "/" + "IndroundProd.csv")
 
 # Write all sheets to csv files
 for key in tqdm(gfpmx_data.keys()):
-    gfpmx_data[key].to_csv(gfpmx_data_dir + "/" + key + ".csv", index=False)
+    df = gfpmx_data[key]
+    # Remove empty columns in place
+    df.dropna(how='all', axis=1, inplace=True)
+    # Write the csv file
+    df.to_csv(gfpmx_data_dir + "/" + key + ".csv", index=False)
