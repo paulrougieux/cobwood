5748bc030dfd6cd33ad4fd2768a3143fe5c23728
Paul Rougieux <paul.rougieux@gmail.com>
2022-01-21 07:18:43 +0100

    Start to compute sawnwood demand

--- scripts/compute_demand.py
+++ scripts/compute_demand.py
@@ -14,24 +14,22 @@ from gftmx.gfpmx_data import gfpmx_data
 # Load sawnwood data
 swd = gfpmx_data.join_sheets('sawn')
 swd.head()
-# Load sawnwood elasticities
-gfpmx_data.get_sheet_wide("sawncons")
 
 # Number of years and number of countries
-n_years = len(swd.year.unique())
-n_countries = len(swd.country.unique())
+n_years = swd.index.to_frame().year.unique()
+n_countries = swd.index.to_frame().country.unique()
 print("Years:", n_years)
 print("Countries:", n_countries)
-print("Lines in the swd data frame:", len(swd), ".",
-      n_years, "*", n_countries, "=", n_years * n_countries)
+print("Number of lines in the swd data frame:", len(swd))
+print("Number of years time the number of countries: ",
+      len(n_years), "*", len(n_countries), "=", len(n_years) * len(n_countries))
 
 
-# swd_cons.drop(columns = [ 'faostat_name', 'price'], inplace=True)
-# 
-# # Compute the consumption equation
-# swd_cons['value2'] = swd_cons.constant * swd_cons['price_lag'].pow(swd_cons.price_elasticity) * \
-#     swd_cons.gdp.pow(swd_cons.gdp_elasticity)
-# 
-# swd_cons['comp_prop'] = swd_cons.value2 / swd_cons.value -1
+# Compute the consumption equation
+# TODO add a loop based on index for the recursive computation of price_{t_1}
+swd["cons2"] = swd["cons_constant"] * swd['cons_price_lag'].pow(swd.cons_price_elasticity) * \
+    swd.cons_gdp.pow(swd.cons_gdp_elasticity)
+
+swd['comp_prop'] = swd_cons.cons2 / swd_cons.cons -1
 # # Don't display rows with NA values
 # swd_cons.query("price_lag==price_lag & gdp_elasticity==gdp_elasticity")
