a2ea757c533a277a09a8fc6b04da8f60670d3ae8
Paul Rougieux <paul.rougieux@gmail.com>
2022-11-20 16:38:54 +0100

    Load PIK data

---
+++ scripts/load_pik_data.py
@@ -0,0 +1,14 @@
+"""
+Load pik scenario data from the paper
+
+- Bodirsky, B.L., Chen, D.MC., Weindl, I. et al. Integrating degrowth and efficiency
+  perspectives enables an emission-neutral food system by 2100. Nat Food 3, 341–348
+  (2022). https://doi.org/10.1038/s43016-022-00500-3
+
+The data is located at:
+
+- https://zenodo.org/record/5543427#.Y3eYOkjMKcM
+
+In the form of GAMMS GDX files (although the model interface seems to be in R).
+
+"""
