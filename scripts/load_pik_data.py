1b8096fd9e75224b03d00b4b08df218eb83473e1
Paul Rougieux <paul.rougieux@gmail.com>
2022-11-24 10:39:35 +0100

    Compare PIK Magpie GDP scenario to World Bank and GFPMx

--- scripts/load_pik_data.py
+++ scripts/load_pik_data.py
@@ -1,5 +1,5 @@
 """
-Load pik scenario data from the paper
+Load PIK-Magpie scenario data from the paper
 
 - Bodirsky, B.L., Chen, D.MC., Weindl, I. et al. Integrating degrowth and efficiency
   perspectives enables an emission-neutral food system by 2100. Nat Food 3, 341–348
@@ -9,6 +9,151 @@ The data is located at:
 
 - https://zenodo.org/record/5543427#.Y3eYOkjMKcM
 
-In the form of GAMMS GDX files (although the model interface seems to be in R).
+The readme says "unit: population: Mio people, gdp: Million USD,"
+
+The data in the figures/scenariogdx folder is in the form of GAMMS GDX files
+(although the model interface seems to be in R). There are also csv files in
+the figures/incomes folder.
+
+Run this script at the command line with
+
+    ipython -i ~/repos/gftmx/scripts/load_pik_data.py
+
+# Draft mail to Bodirsky
+
+Dear
+I am implementing a forest sector model and would like to test it on your scenarios.
+
+I tried looking at DegrowthMAgPIE/figures/incomes
+but I have trouble understanding the content of the files
+The readme says "unit: population: Mio people, gdp: Million USD,"
+and the files have
+
 
 """
+
+from pathlib import Path
+import numpy as np
+import pandas
+import matplotlib.pyplot as plt
+import seaborn
+
+# import gdxpds
+from gftmx.gfpmx_data import gfpmx_data
+
+# To get country ISO codes
+from biotrade.faostat import faostat
+from biotrade.world_bank import world_bank
+import re
+
+country_iso_codes = faostat.country_groups.df[["fao_table_name", "iso3_code"]].rename(
+    columns={"fao_table_name": "country", "iso3_code": "country_iso"}
+)
+
+# Requires a working version of GAMMS
+# dataframes = gdxpds.to_dataframes(magpie_scenario_path / "bau_fulldata.gdx")
+# for symbol_name, df in dataframes.items():
+#     print("Doing work with {}.".format(symbol_name))
+
+# Load Magpie income data
+scenario_path = Path("~/large_models/DegrowthMAgPIE/figures/scenariogdx")
+incomes_path = Path("~/large_models/DegrowthMAgPIE/figures/incomes")
+bau_gdp = pandas.read_csv(incomes_path / "bau_gdp_ppp_iso.csv")
+bau_gdp.rename(columns=lambda x: re.sub(r" ", "_", str(x)).lower(), inplace=True)
+bau_gdp.rename(columns={"region": "country_iso"}, inplace=True)
+bau_gdp["year"] = pandas.to_numeric(bau_gdp["year"].str.replace("y", ""))
+
+# Load GFPMX GDP data
+# Expressed in 1000 USD of 2017
+gfpm_gdp = gfpmx_data.get_sheet_long("gdp").merge(
+    country_iso_codes, on="country", how="left"
+)
+# Convert from 1000 USD to million USD
+gfpm_gdp["gdp"] = gfpm_gdp["gdp"] / 1e3
+gfpm_gdp.rename(columns={"gdp": "gfpm_gdp"}, inplace=True)
+
+# Load World bank GDP data in PPP constant USD of 2017
+# https://data.worldbank.org/indicator/NY.GDP.MKTP.PP.KD
+wb_gdp = world_bank.db.select("indicator", indicator_code="NY.GDP.MKTP.PP.KD")
+wb_gdp.rename(columns={"reporter_code": "country_iso", "value": "wb_gdp"}, inplace=True)
+# Convert from USD to million USD
+wb_gdp["wb_gdp"] = wb_gdp["wb_gdp"] / 1e6
+
+
+# Max GDP per region
+bau_gdp.loc[bau_gdp.groupby("country_iso")["ssp2"].idxmax()]
+bau_gdp.query("country_iso=='FRA'")
+
+# Check that there are no NA values for EU country ISO codes
+selector = gfpm_gdp["country"].isin(faostat.country_groups.eu_country_names)
+assert not any(gfpm_gdp[selector]["country_iso"].isna())
+
+# Merge GFPM, Magpie and world bank data and compare the GDP of EU countries in 2030
+index = ["country_iso", "year"]
+comp = (
+    gfpm_gdp[index + ["country", "gfpm_gdp"]]
+    .merge(bau_gdp[index + ["ssp2"]], on=index, how="left")
+    .merge(wb_gdp[index + ["wb_gdp"]], on=index, how="left")
+)
+selector = comp["country"].isin(faostat.country_groups.eu_country_names)
+compeu = comp[selector].copy()
+
+# Reshape to long format
+compeu_long = compeu.melt(
+    id_vars=["country_iso", "year", "country"], var_name="source", value_name="gdp"
+)
+
+# Plot
+# index = ["country_iso", "year", "country"]
+# comp.set_index("year").plot(title="Compare MagPie do World Bank")
+# plt.show()
+
+
+#######################
+# XY comparison plots #
+#######################
+compeu["country"] = compeu["country"].astype("category")
+g = seaborn.FacetGrid(
+    compeu.query("not ssp2.isna()"),
+    col="country",
+    col_wrap=6,
+    sharex=False,
+    sharey=False,
+)  # , height=6)
+g.map_dataframe(seaborn.scatterplot, x="gfpm_gdp", y="ssp2", hue="country")
+# From https://stackoverflow.com/questions/54390054/how-to-add-a-comparison-line-to-all-plots-when-using-seaborns-facetgrid
+
+
+def const_line(*args, **kwargs):
+    x = np.arange(0, 1e7, 1e6)
+    plt.plot(x, x)
+
+
+g.map(const_line)
+plt.show()
+
+
+#################################
+# X along time comparison plots #
+#################################
+# GDP in billion USD
+compeu_long["gdp_b"] = compeu_long["gdp"] / 1e3
+g = seaborn.relplot(
+    data=compeu_long,
+    x="year",
+    y="gdp_b",
+    col="country",
+    col_wrap=7,
+    hue="source",
+    style="source",
+    kind="line",
+    height=3,
+    facet_kws={"sharey": False, "sharex": False},
+)
+g.fig.supylabel("GDP in billion USD")
+g.fig.subplots_adjust(left=0.05)
+g.set(ylim=(0, None))
+plt.savefig("/tmp/comp_gdp_by_country.pdf")
+
+
+# Compute yearly increase by variable
