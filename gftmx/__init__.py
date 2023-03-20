ad88fd85c995a8f495e2c8897e84a04383c1470b
Paul Rougieux <paul.rougieux@gmail.com>
2023-03-20 15:21:16 +0100

    Simplify the name of the the data folder

--- gftmx/__init__.py
+++ gftmx/__init__.py
@@ -9,8 +9,12 @@ from pathlib import Path
 import os
 
 # Where is the data, default case #
-gftmx_data_dir = Path("~/repos/gftmx_data/")
+data_dir = Path("~/repos/gftmx_data/")
 
 # But you can override that with an environment variable #
 if os.environ.get("GFTMX_DATA"):
-    gftmx_data_dir = Path(os.environ["GFTMX_DATA"])
+    data_dir = Path(os.environ["GFTMX_DATA"])
+
+# For backward compatibility
+# TODO: remove when it is replaced everywhere by gftmx.data_dir
+gftmx_data_dir = data_dir
