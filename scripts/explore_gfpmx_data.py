2c56869097dffdc80f2c799c576795d345edeca0
Paul Rougieux <paul.rougieux@gmail.com>
2023-03-20 15:20:43 +0100

    GFPMx compute EU harvest increases by 2030 and 2050

--- scripts/explore_gfpmx_data.py
+++ scripts/explore_gfpmx_data.py
@@ -1,11 +1,18 @@
 """ The purpose of this script is to compute the percentage increase of roundwood harvest by 2050
 
+Run this script at the command line with:
+
+    ipython -i ~/repos/gftmx/scripts/explore_gfpmx_data.py
+
+
 - Compute the aggregate for EU countries of production and consumption.
 - Compute the percentage change between any given year.
 
 """
-from gftmx.gfpmx_data import gfpmx_data
+import numpy as np
+import pandas
 from biotrade.faostat import faostat
+from gftmx.gfpmx_data import gfpmx_data
 
 round_ = gfpmx_data.get_country_rows("round")
 fuel = gfpmx_data.get_country_rows("fuel")
@@ -13,6 +20,7 @@ indround = gfpmx_data.get_country_rows("indround")
 
 # Select EU countries
 eucountries = faostat.country_groups.eu_country_names + ["Czech Republic"]
+roundeu = round_.query("country in @eucountries")
 indroundeu = indround.query("country in @eucountries")
 fueleu = fuel.query("country in @eucountries")
 
@@ -33,6 +41,7 @@ def print_increase_in_production(df, var, start, end):
     )
 
 
+print_increase_in_production(roundeu, "prod", 2020, 2050)
 print_increase_in_production(indroundeu, "prod", 2020, 2050)
 print_increase_in_production(fueleu, "prod", 2020, 2050)
 
@@ -41,3 +50,15 @@ print_increase_in_production(indroundeu, "prod", 2022, 2050)
 
 print_increase_in_production(indroundeu, "cons", 2020, 2050)
 print_increase_in_production(fueleu, "cons", 2020, 2050)
+
+
+# Export summary tables to a spreadsheet
+roundprodagg = roundeu.groupby("year")["prod"].agg(sum).rename("round")
+indroundprodagg = indroundeu.groupby("year")["prod"].agg(sum).rename("indround")
+fuelprodagg = fueleu.groupby("year")["prod"].agg(sum).rename("fuel")
+# TODO replace this with a function and list comprehension
+agg = pandas.concat([roundprodagg, indroundprodagg, fuelprodagg], axis=1)
+np.testing.assert_allclose(agg["round"], agg["indround"] + agg["fuel"])
+
+agg_selected = agg.query("year in [2010,2015,2030,2050]").transpose()
+# (agg_selected / 1e3).to_csv("/tmp/gfpmxeuagg_selected.csv")
