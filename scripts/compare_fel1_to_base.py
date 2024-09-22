a6c89ed0505493af30d11341b18a8ddfc9aca281
Paul Rougieux <paul.rougieux@gmail.com>
2024-09-22 09:53:46 +0200

    Compare fuel and indround cons prod change when elasticities change

--- scripts/compare_fel1_to_base.py
+++ scripts/compare_fel1_to_base.py
@@ -2,13 +2,17 @@
 """
 
 import sys
-from cobwood.gfpmx import GFPMX
+import pandas
+import matplotlib.pyplot as plt
 from biotrade.faostat import faostat
+from eu_cbm_hat import eu_cbm_data_pathlib
+from cobwood.gfpmx import GFPMX
 import cobwood
 
-# Load CBM output data
-from eu_cbm_hat import eu_cbm_data_pathlib
+eu_countries = faostat.country_groups.eu_country_names
+eu_countries += ["Netherlands"]
 
+# Load CBM output data
 scenario_dir = (
     eu_cbm_data_pathlib.parent / "eu_cbm_explore" / "scenarios" / "ssp2_fair_degrowth"
 )
@@ -29,12 +33,78 @@ gfpmx_pikssp2_fel1 = GFPMX(
 gfpmx_pikssp2_base = GFPMX(
     input_dir="gfpmx_base2021", base_year=2021, scenario_name="pikssp2"
 )
+gfpmx_pikfair_fel1 = GFPMX(
+    input_dir="gfpmx_base2021", base_year=2021, scenario_name="pikfair_fel1"
+)
+gfpmx_pikfair_base = GFPMX(
+    input_dir="gfpmx_base2021", base_year=2021, scenario_name="pikfair"
+)
 
 
-# Comparison of EU fuelwood consumption and production
+def get_df_eu(model, product, variable):
+    """Extract a data frame from the model xarray output keep only EU countries"""
+    df = model[product][variable].to_dataframe().reset_index()
+    selector = df["country"].isin(eu_countries)
+    df = df.melt(id_vars=["country", "year"], value_vars="cons", var_name="variable")
+    df["product"] = product
+    df["scenario"] = model.scenario_name
+    return df.loc[selector].reset_index(drop=True)
 
-gfpmx_pikssp2_base["fuel"]["cons"]
+
+########################################################
+# Comparison of EU fuelwood consumption and production #
+########################################################
+comp_list = []
+for this_prod in ["fuel", "indround"]:
+    for this_var in ["cons", "prod"]:
+        dfssp2base = get_df_eu(gfpmx_pikssp2_base, this_prod, this_var)
+        dfssp2fel1 = get_df_eu(gfpmx_pikssp2_fel1, this_prod, this_var)
+        dffairbase = get_df_eu(gfpmx_pikfair_base, this_prod, this_var)
+        dffairfel1 = get_df_eu(gfpmx_pikfair_fel1, this_prod, this_var)
+        comp_list += [dfssp2base, dfssp2fel1, dffairbase, dffairfel1]
+
+comp = pandas.concat(comp_list)
+del comp_list
+index = ["year", "product", "scenario", "variable"]
+compeu = comp.groupby(index)["value"].agg("sum").reset_index()
 
 
-# Comparison of total roundwood consumption and production
-# Including fuelwood and industrial roundwood
+########
+# Plot #
+########
+# Facet plot by country with 2 colors for the different sources
+def plotcomp(df, product, variable):
+    selector = df["product"] == product
+    selector &= df["variable"] == variable
+    selector &= df["year"] != 2071
+    df = df.loc[selector].copy()
+    df["value"] *= 1e-6
+    df_wide = df.pivot(columns="scenario", index="year", values="value")
+    df_wide.plot(title=f"{product} {variable} Million m3")
+    plt.show()
+
+
+plotcomp(compeu, "fuel", "cons")
+
+plotcomp(compeu, "fuel", "prod")
+plotcomp(compeu, "indround", "prod")
+plotcomp(compeu, "indround", "cons")
+
+# Percentage difference between base and fel1
+# In 2030, 2050 and 2070
+compeu["base_or_fel1"] = "base"
+compeu[["scenario2", "model_version"]] = compeu["scenario"].str.split(
+    "_", n=1, expand=True
+)
+selector = compeu["scenario"].str.contains("_fel1")
+compeu.loc[~selector, "model_version"] = "base"
+
+index = ["year", "product", "variable", "scenario2"]
+compeu_wide = compeu.pivot(columns="base_or_fel1", index=index, values="value")
+compeu_wide["reldiff"] = compeu_wide["fel1"] / compeu_wide["base"] - 1
+compeu_wide.query("year in [2030, 2050]")
+
+# Roundwood = indround + Fuelwood 2050
+#                                   base	fel1	reldiff
+# indround	cons	pikfair	471085.040325443	415235.488134672	-0.118555138478157
+# indround	prod	pikssp2	633721.456237559	649287.283194183	0.0245625689384723
