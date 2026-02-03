a41cc94b207d39bae49895005f5c166440189856
Paul Rougieux <paul.rougieux@gmail.com>
2026-02-03 16:59:31 +0100

    Initiate FAOSTAT dataset in Xarray format

--- cobwood/faostat/__init__.py
+++ cobwood/faostat/__init__.py
@@ -3,13 +3,12 @@
 
 Note:
 
-- The problem is that `faostat.forestry_production_df` requires the
+- `faostat.forestry_production_df` requires the
   [biotrade](https://pypi.org/project/biotrade/) package and the
   `biotrade_data` directory to be defined so that the intermediate download can
   be saved.
 
 """
-
 from functools import cached_property
 from biotrade.faostat import faostat as biotrade_faostat
 import pandas as pd
@@ -17,11 +16,12 @@ import xarray as xr
 
 
 class FAOSTAT:
-    """Download faostat data and prepare it as N dimentional Xarray data
+    """Download faostat data and prepare it as N dimentional
+    Xarray data
 
-    >>> from cobwood.faostat import faostat
-    >>> fp = faostat.forestry_production_df
-    >>> faostat.forestry_production_ds
+    from cobwood.faostat import faostat
+    fp = faostat.forestry_production_df
+    ds = faostat.forestry_production_ds
 
     TODO: Make a dataset for sawnwood elements
 
@@ -30,11 +30,10 @@ class FAOSTAT:
     selector = fp["product"] == "sawnwood"
     df = fp.loc[selector].copy()
     for element in df["element"].unique():
-        selector = fp["element"] == element
-        df_var = df.loc[element]
         print(element)
+        selector = df["element"] == element
+        df_e = df.loc[selector]
         # Add the data array to the dataset
-
     """
 
     def __init__(self):
@@ -45,8 +44,20 @@ class FAOSTAT:
         """Forestry production pandas dataframe"""
         return biotrade_faostat.pump.read_df("forestry_production")
 
-    def foresty_production_ds(self) -> xr.Dataset:
+    @cached_property
+    def forestry_production_ds(self) -> xr.Dataset:
         """Forestry production Xarray Dataset"""
+        df = self.forestry_production_df
+        ds = xr.Dataset()
+        for element in df["element"].unique():
+            # TODO: replace the product names by shorter product names usable as modelling variables.
+            # There should be a mapping to short names and product codes.
+            df_elem = df[df["element"] == element].set_index(
+                ["reporter", "product", "year"]
+            )["value"]
+            da = df_elem.to_xarray()
+            ds[element] = da
+        return ds
 
 
 # Make a singleton
